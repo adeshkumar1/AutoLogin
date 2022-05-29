4c44d707d51943cf9d60ea94385107bce5c426c3
adeshkumar1 <89107712+adeshkumar1@users.noreply.github.com>
2022-05-29 14:49:38 -0500

    sdffgsdg

---
+++ AccountSwitcher.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+import subprocess
+import time
+import pyautogui
+
+root = tk.Tk()
+root.title('Valorant Account Selector')
+Loc = 0
+accounts = []
+user = tk.StringVar()
+pas = tk.StringVar()
+
+if os.path.isfile('sve.txt'):
+    with open('sve.txt', 'r') as f:
+        tempApps = f.read()
+        tempApps = tempApps.split(',')
+        accounts = [x for x in tempApps if x.strip()]
+with open('Loc.txt','r') as f:
+    Loc = f.read() 
+
+
+def login(username,password):
+    subprocess.Popen(Loc)
+    time.sleep(5)
+    pyautogui.typewrite(username, interval = 0.01)
+    pyautogui.press('tab')
+    pyautogui.typewrite(password,interval = 0.01)
+    pyautogui.press('enter')
+    time.sleep(3)
+    #selects valorant
+    pyautogui.moveTo(445,445)
+    pyautogui.click()
+    time.sleep(2)
+    #clicks play button
+    pyautogui.moveTo(330,920)
+    pyautogui.click()   
+def submit():
+    account = tk.Button(frame, text = user.get())
+    account.pack()
+    accounts.append(user.get() + ' '+ pas.get())
+    
+
+
+
+canvas = tk.Canvas(root, height = 700, width = 500, bg = "#263D42")
+canvas.pack()
+
+frame = tk.Frame(root, bg = "white")
+frame.place(relwidth = 0.8, relheight = 0.8,relx = 0.1,rely=0.1)
+
+
+
+
+
+name_label = tk.Label(root, text = 'Username', font=('calibre',10, 'bold'))
+name_entry = tk.Entry(root,textvariable = user, font=('calibre',10,'normal'))
+
+passw_label = tk.Label(root, text = 'Password', font = ('calibre',10,'bold'))
+passw_entry=tk.Entry(root, textvariable = pas, font = ('calibre',10,'normal'), show = '*')
+
+sub_btn=tk.Button(root,text = 'Submit', command = submit)
+
+name_label.pack()
+name_entry.pack()
+passw_label.pack()
+passw_entry.pack()
+sub_btn.pack()
+
+for account in accounts:
+    label = tk.Button(frame, text = account)
+    label.pack()
+
+if Loc == 0:
+    
+    Loc = filedialog.askopenfilename(initialdir = "/",title = "Select File",
+    filetypes = (("executables" , "*.exe"),("all files","*.*")))
+    with open('Loc.txt','w') as f:
+        f.write(Loc)
+
+
+root.mainloop()
+
+with open('sve.txt', 'w') as f:
+    for account in accounts:
+        f.write(account+',')
+
+    
